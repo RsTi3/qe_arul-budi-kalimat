546e8341fc6098965c568cbe3087c12aa8f38ca2
Arul Budi Kalimat <97519455+RsTi3@users.noreply.github.com>
2023-03-09 09:58:07 +0700

    (doc) sec5 : Menambahkan File Praktikum
    Menambahkan file praktikum tugas Basic Linux Command Section 5

---
+++ 5_Basic_Linux_Command/Praktikum/arul.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Halo Semuanya Selamat Datang !"
+echo "Siapakah Nama Anda ? Jawab ya : "
+read nama
+echo "Hai , $nama ! kamu memiliki nama yang sangat bagus"
+echo "Asalkan kau tahu, hari ini yang sangat indah. Untuk hari kau bisa melihat dibawah ini : "
+date
+echo "Kau Tahu ? Saat ini kau sedang menggunakan direktori file : "
+ls
